d17e7ec988c4453e7f1c589f05bad8818ba32221
maxlo <maxloginov2001@mail.ru>
2023-03-14 15:02:27 +0300

    commit channel.py and youtube_api_manual.py

--- src/channel.py
+++ src/channel.py
@@ -3,9 +3,12 @@
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id: str, title: str, video_count: int, url: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.title = title
+        self.video_count = video_count
+        self.url = url
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
