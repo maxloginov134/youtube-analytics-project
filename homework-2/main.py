4f0772d4973b4c1eca0ed64f5dc7951fd8b41d33
maxlo <maxloginov2001@mail.ru>
2023-03-18 02:44:47 +0300

    commit channel.py and main.py

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,7 +1,7 @@
 from src.channel import Channel
 
 if __name__ == '__main__':
-    vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+    vdud = Channel('UCU_yU4xGT9hrVFo6euH8LLw')
 
     # получаем значения атрибутов
     print(vdud.title)  # вДудь
@@ -9,11 +9,11 @@ if __name__ == '__main__':
     print(vdud.url)  # https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA
 
     # менять не можем
-    vdud.channel_id = 'Новое название'
+    # vdud.channel_id = 'Новое название'
     # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
     # можем получить объект для работы с API вне класса
-    print(Channel.get_service())
+    # print(Channel.get_service())
     # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
 
     # создаем файл 'vdud.json' в данными по каналу
--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,6 @@
 from googleapiclient.discovery import build
 import os
+import json
 
 
 class Channel:
@@ -10,21 +11,28 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        # self.title = title
-        # self.video_count = video_count
-        # self.url = url
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
+        self.url = f"https://www.youtube.com/channel/{channel_id}"
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.subscribers_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
+        self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])
 
-    def print_info(self) -> str:
+    def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute())
 
-    # def to_json(self, filename: str):
-    # json_object = json.dumps({
-    #  "id": self.channel_id,
-    # "title": self.title,
-    # "video_count": self.video_count,
-    # "url": self.url
-    # }, indent=4)
+    def to_json(self, filename: str):
 
-    # with open(filename, "w") as outfile:
-    #   outfile.write(json_object)
+        json_object = {
+          "id": self.channel_id,
+          "title": self.title,
+          "video_count": self.video_count,
+          "url": self.url,
+          "subscribers_count": self.subscribers_count,
+          "view_count": self.view_count
+        }
+
+        with open(filename, "w") as file:
+            json.dump(json_object, file, indent=4)
